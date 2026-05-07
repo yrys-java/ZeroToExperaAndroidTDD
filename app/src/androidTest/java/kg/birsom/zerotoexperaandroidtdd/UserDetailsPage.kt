bc1032615ecbf19e45da309d807c86c93b7de3d0
b1rsom <rysnurllanov26@gmail.com>
2026-05-07 16:52:37 +0600

    test: add user details content ui scenario

---
+++ app/src/androidTest/java/kg/birsom/zerotoexperaandroidtdd/UserDetailsPage.kt
@@ -0,0 +1,69 @@
+package kg.birsom.zerotoexperaandroidtdd
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertTextEquals
+import androidx.compose.ui.test.junit4.ComposeTestRule
+import androidx.compose.ui.test.onNodeWithTag
+
+class UserDetailsPage(
+    private val composeTestRule: ComposeTestRule
+) {
+
+    fun assertContent(
+        name: String,
+        username: String,
+        email: String,
+        phone: String,
+        website: String,
+        address: String,
+        company: String
+    ) {
+        composeTestRule.onNodeWithTag(UserDetailsScreenTags.BUTTON_DETAILS_BACK)
+            .assertIsDisplayed()
+
+        composeTestRule.onNodeWithTag(
+            testTag = UserDetailsScreenTags.TEXT_DETAILS_NAME,
+            useUnmergedTree = true
+        ).assertTextEquals(name)
+
+        composeTestRule.onNodeWithTag(
+            testTag = UserDetailsScreenTags.TEXT_DETAILS_USERNAME,
+            useUnmergedTree = true
+        ).assertTextEquals(username)
+
+        composeTestRule.onNodeWithTag(
+            testTag = UserDetailsScreenTags.TEXT_DETAILS_EMAIL,
+            useUnmergedTree = true
+        ).assertTextEquals(email)
+
+        composeTestRule.onNodeWithTag(
+            testTag = UserDetailsScreenTags.TEXT_DETAILS_PHONE,
+            useUnmergedTree = true
+        ).assertTextEquals(phone)
+
+        composeTestRule.onNodeWithTag(
+            testTag = UserDetailsScreenTags.TEXT_DETAILS_WEBSITE,
+            useUnmergedTree = true
+        ).assertTextEquals(website)
+
+        composeTestRule.onNodeWithTag(
+            testTag = UserDetailsScreenTags.TEXT_DETAILS_ADDRESS,
+            useUnmergedTree = true
+        ).assertTextEquals(address)
+
+        composeTestRule.onNodeWithTag(
+            testTag = UserDetailsScreenTags.TEXT_DETAILS_COMPANY,
+            useUnmergedTree = true
+        ).assertTextEquals(company)
+    }
+
+    fun assertLoadingDoesNotExist() {
+        composeTestRule.onNodeWithTag(UserDetailsScreenTags.BOX_DETAILS_LOADING)
+            .assertDoesNotExist()
+    }
+
+    fun assertErrorDoesNotExist() {
+        composeTestRule.onNodeWithTag(UserDetailsScreenTags.CARD_DETAILS_ERROR)
+            .assertDoesNotExist()
+    }
+}
---
+++ app/src/androidTest/java/kg/birsom/zerotoexperaandroidtdd/UserDetailsScreenUiTest.kt
@@ -0,0 +1,55 @@
+package kg.birsom.zerotoexperaandroidtdd
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import kg.birsom.zerotoexperaandroidtdd.feature.users.presentation.detail.state.UserDetailsUi
+import kg.birsom.zerotoexperaandroidtdd.feature.users.presentation.detail.state.UserDetailsUiState
+import kg.birsom.zerotoexperaandroidtdd.ui.theme.ZeroToExperaAndroidTDDTheme
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class UserDetailsScreenUiTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    private val detailsPage = UserDetailsPage(composeTestRule)
+
+    @Test
+    fun shows_user_details_content_without_loading_and_error() {
+        composeTestRule.setContent {
+            ZeroToExperaAndroidTDDTheme {
+                UserDetailsScreen(
+                    uiState = UserDetailsUiState.Content(
+                        user = UserDetailsUi(
+                            id = 1,
+                            name = "Leanne Graham",
+                            username = "Bret",
+                            email = "Sincere@april.biz",
+                            phone = "1-770-736-8031 x56442",
+                            website = "hildegard.org",
+                            address = "Kulas Light, Apt. 556, Gwenborough, 92998-3874",
+                            company = "Romaguera-Crona"
+                        )
+                    ),
+                    onBackClick = {},
+                    onRetryClick = {}
+                )
+            }
+        }
+
+        detailsPage.assertContent(
+            name = "Leanne Graham",
+            username = "@Bret",
+            email = "Sincere@april.biz",
+            phone = "1-770-736-8031 x56442",
+            website = "hildegard.org",
+            address = "Kulas Light, Apt. 556, Gwenborough, 92998-3874",
+            company = "Romaguera-Crona"
+        )
+        detailsPage.assertLoadingDoesNotExist()
+        detailsPage.assertErrorDoesNotExist()
+    }
+}
